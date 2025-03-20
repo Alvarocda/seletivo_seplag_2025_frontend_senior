4dbc7ab74cb3627811812cd1b61cbc689ff52ab1
Álvaro Claro <alvarocda@gmail.com>
2025-03-20 01:33:33 +0000

    commit inicial

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, MatFormFieldModule, MatInputModule, MatButtonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'seletivo';
+}
