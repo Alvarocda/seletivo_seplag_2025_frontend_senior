f5b910ff25553a3d5cfa4518b08fa76bf71fd065
Álvaro Claro <alvarocda@gmail.com>
2025-03-26 02:47:20 +0000

    Finalizado tela de 404

--- src/app/modules/not-found/not-found.module.ts
+++ src/app/modules/not-found/not-found.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { NotFoundComponent } from './container/not-found.component';
 import { Router, RouterModule } from '@angular/router';
 import { routes } from './not-found.routes';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(routes), MatButtonModule],
   exports: [],
   declarations: [NotFoundComponent],
   providers: [],
