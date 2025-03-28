6175ff63bbbaf55a2bf77323dd8c56e707d83db2
Álvaro Claro <alvarocda@gmail.com>
2025-03-28 01:08:24 +0000

    Melhorias de UI/UX

--- src/app/modules/home/components/home-card/home-card.component.ts
+++ src/app/modules/home/components/home-card/home-card.component.ts
@@ -3,21 +3,25 @@ import { IDesaparecido, ListaSexos } from '../../home.types';
 import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-home-card',
-    templateUrl: 'home-card.component.html',
-    standalone: false
+  selector: 'app-home-card',
+  templateUrl: 'home-card.component.html',
+  standalone: false,
 })
+export class HomeCardComponent {
+  listaSexos = ListaSexos;
 
-export class HomeCardComponent{
-    listaSexos = ListaSexos;
+  carregandoImagem: boolean = true;
 
-    @Input({required: true})
-    desaparecido!: IDesaparecido;
+  @Input({ required: true })
+  desaparecido!: IDesaparecido;
 
-    constructor(private _router: Router){}
+  constructor(private _router: Router) {}
 
-    detalharDesaparecido() {
-        this._router.navigate(['detalhes-desaparecido', this.desaparecido.id])
-    }
+  detalharDesaparecido() {
+    this._router.navigate(['detalhes-desaparecido', this.desaparecido.id]);
+  }
 
+  toggleCarregandoImagem(carregando: boolean) {
+    this.carregandoImagem = carregando;
+  }
 }
